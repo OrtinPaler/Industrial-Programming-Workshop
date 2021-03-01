19fc5d8b472dc8fab1821dbead9193b4ec4db4b2
Mikhail Misharin <ortin@Venus.lan>
2021-03-01 23:01:10 +0300

    complete lab_1

---
+++ WorkMatrix.h
@@ -0,0 +1,16 @@
+// фукнция для изменения текущей матрицы
+void change_numMatrix(int&);
+// функция для ввода параметров матрицы
+void enterMatrixParameters(int&, int&, int&);
+// функция для создания матрицы
+int** createMatrix(int, int);
+// функция для выбора способа заполнения матрицы
+int chooseFillingMethod();
+// функция для заполнения матрицы
+void fillingMatrix(int, int, int**, int);
+// функция для вывода матрицы
+void printMatrix(int, int, int**, int&);
+// функция для копирования значений одной матрицы в другую
+void copyValues(int**, int**, int, int, int, int);
+// функция для создания подматрицы
+int** submatrixPartition(int, int, int, int, int**);
